401194e7a6cc91a0db0c28143feb806b6961fa41
Yolanda Barrera <136383160+YolandaBarrera@users.noreply.github.com>
2026-02-14 18:16:23 -0600

    Add files via upload
    Programa que recupera la edad y muestra el mensaje si es mayor de edad o menor de edad.

---
+++ Edad/src/edad/Edad.java
@@ -0,0 +1,51 @@
+package edad;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Edad extends JFrame implements ActionListener {
+
+    JTextField numUno;
+    JButton btnOperar;
+    JPanel panel01;
+
+    public Edad() {
+        setTitle("Mayor o Menor de Edad");
+        setSize(400,200);
+        setDefaultCloseOperation(3);
+               
+        numUno = new JTextField(5);
+        btnOperar = new JButton("Aceptar");
+        btnOperar.addActionListener(this);
+       
+        panel01 = new JPanel();
+       
+        panel01.add(numUno);
+        panel01.add(btnOperar);
+       
+        add(panel01);
+        setVisible(true);
+    }
+   
+    public static void main(String[] args) {
+        Edad e = new Edad();        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String n1 = numUno.getText();
+       
+        int edad = Integer.parseInt(n1);        
+       
+        if(edad >= 18){
+            JOptionPane.showMessageDialog(null, "Eres mayor de edad");
+        }else{
+            JOptionPane.showMessageDialog(null, "Eres menor de edad");
+        }
+     }
+}
